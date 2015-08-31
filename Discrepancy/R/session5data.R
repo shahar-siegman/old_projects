99dd7c32c012322244f96c64d5dba9d809e15faf
shahar.siegman <shahar@komoona.com>
2015-08-31 18:59:48 +0300

    finished writing, start debugging
    Former-commit-id: cb65d3545089734669b4e55182e97475a0ea46a7

--- Discrepancy/R/session5data.R
+++ Discrepancy/R/session5data.R
@@ -1,25 +1,32 @@
+getRawPlacementData <- function() {
+  rawDF<-read.csv("C:/Shahar/Projects/Discrepancy/raw_input/data.txt")
+  rawDF$Date <- as.Date(rawDF$Date,format="%d/%m/%Y")
+  return(rawDF)
+}
+
 dByPercentileByLength <- function() {
-  return (matrix(data=c(0.02217738,  0, 0, 0.033033396, 0.040871774,
-                0.037390663, 0.051550697, 0.056261989, 0.070640604, 0.064655012,
-                0.046207342, 0.069245461, 0.072188022, 0.081182855, 0.100386763,
-                0.058091496, 0.08646321, 0.101190576, 0.135233429, 0.116811584,
-                0.075231413, 0.101153874, 0.118095289, 0.137355021, 0.128805942,
-                0.080398846, 0.094521006, 0.12951405, 0.13439563, 0.156126689,
-                0.075334121, 0.118872192, 0.133850714, 0.162576372, 0.172941469,
-                0.094847915, 0.120612949, 0.14654267, 0.176608815, 0.197229507,
-                0.09277559, 0.137898533, 0.185141754, 0.209088007, 0.232806957,
-                0.129129403, 0.172786727, 0.208996453, 0.236350137, 0.271494936,
-                0.116629786, 0.182388126, 0.228793711, 0.28245799, 0.283195773,
-                0.176345442, 0.231528173, 0.303243233, 0.35616288, 0.381108087,
-                0.268501886, 0.322640367, 0.391877968, 0.467791911, 0.503797605,
-                0.532425846, 0.548081114, 0.899516676, 0.843405534, 0.902956858
-                ),nrow=5,byrow=FALSE))
+  # each row: a particular chain length
+  # each column: different category
+  return (matrix(data=c(0.02217738, 0, 0, 0.033033396, 0.040871774,
+                        0.037390663, 0.051550697, 0.056261989, 0.070640604, 0.064655012,
+                        0.046207342, 0.069245461, 0.072188022, 0.081182855, 0.100386763,
+                        0.058091496, 0.08646321, 0.101190576, 0.13439563, 0.116811584,
+                        0.075231413, 0.094521006, 0.118095289, 0.135233429, 0.128805942,
+                        0.075334121, 0.101153874, 0.12951405, 0.137355021, 0.156126689,
+                        0.080398846, 0.118872192, 0.133850714, 0.162576372, 0.172941469,
+                        0.09277559, 0.120612949, 0.14654267, 0.176608815, 0.197229507,
+                        0.094847915, 0.137898533, 0.185141754, 0.209088007, 0.232806957,
+                        0.116629786, 0.172786727, 0.208996453, 0.236350137, 0.271494936,
+                        0.129129403, 0.182388126, 0.228793711, 0.28245799, 0.283195773,
+                        0.176345442, 0.231528173, 0.303243233, 0.35616288, 0.381108087,
+                        0.268501886, 0.322640367, 0.391877968, 0.467791911, 0.503797605,
+                        0.532425846, 0.548081114, 0.899516676, 0.843405534, 0.902956858),nrow=5,byrow=FALSE))
 }
 
 fitAlongChainLength <- function (dByPercentileByLengthData) {
+  # output a linear model (by chain length) per category
   R=nrow(dByPercentileByLengthData) # 5
   C=ncol(dByPercentileByLengthData) # 14
-  ptiles=1:C/C- 1/(2*C)
   d1<-data.frame(y=dByPercentileByLengthData[,1],x=seq(1,R))
   fit1 <- lm(y~x, data=d1)
   b=coefficients(fit1)
@@ -34,6 +41,7 @@ fitAlongChainLength <- function (dByPercentileByLengthData) {
 
 fitAlongPercentiles <- function(b) {
   # fit an exponential model to the intercepts and to the linear coefficients
+  # of the individual category models
   C=nrow(b)
   ptiles=1:C/C- 1/(2*C)
   d2 <- data.frame(intercept=b[,1],linear=b[,2], x=ptiles)
@@ -42,54 +50,91 @@ fitAlongPercentiles <- function(b) {
   return(rbind(coefficients(fitIntercept),coefficients(fitLinear)))
 }
 
-mapPlacementsToPercentiles <- function(placementData) {
-  R=nrow(placementData)
-  b=vector(length=R)
-  for (i in 1:R) {
-    b[i]=findInterval(placementData$DiscrepancyPercent,d[placementData$ChainLength,])
-  }
-}
-
-getRawPlacementData <- function() {
-  df1<-read.csv("C:/Shahar/Projects/Discrepancy/raw_input/data.txt")
-  df1$Date <- as.Date(df1$Date,format="%d/%m/%Y")
-  return(df1)
-}
-
-getSamplePlacementData <- function (df) {
- res <- df %>%
-    filter(Date > "2015-07-01" & Date < "2015-07-09") %>%
+filterAndGroupRawData <- function (df, startDate, endDate) {
+  # aggregate data for a certain date range by placement and chain length
+  res <- df %>%
+    filter(Date >= startDate & Date < endDate) %>%
     group_by(placementId,ChainLength) %>%
     select(Impressions,Served,House) %>%
     summarise(
       sumImps=sum(Impressions),
       sumServed=sum(Served),
-      sumHouse=sum(House))   #  filter( Date>"2015-07-08" & Date<"2015-07-15")
- res$DiscrepancyPercent=(res$sumImps-res$sumServed-res$sumHouse)/res$sumImps
- return(res)
+      sumHouse=sum(House))
+
+  res$DiscrepancyPercent=(res$sumImps-res$sumServed-res$sumHouse)/res$sumImps
+  return(res)
 }
 
-getDiscrepancyModel <- function(dByPlacementByLength) {
-  # get the percentile of each placement-length
+getDiscrepancyModel <- function(groupedDF) {
+  # get the model of each placement-length
   d<-dByPercentileByLength()
-  R=nrow(d)
-  c=ncol(d)
-  newVec <- vector(length=R)
+  C=ncol(d)+1
+  dr=nrow(d)
+  t <- fitAlongPercentiles(fitAlongChainLength(d))
+  nLines=nrow(groupedDF)
+
+  newVec <- vector(length=nLines)
   interval <- newVec
   percentile <- newVec
   interceptForCLModel <- newVec
   slopeForCLModel <- newVec
-  t <- fitAlongPercentiles(fitAlongChainLength(d))
-  for (i in 1:R) {
-    interval[i]=findInterval(dByPlacementByLength$DiscrepancyPercent, b[dByPlacementByLength$ChainLength[i],])
-    percentile[i]=interval/C-1/(2*C)
-    interceptForCLModel[i]=t[1,1]+t[1,2]*percentile
-    slopeForCLModel[i]=t[2,1]+t[2,2]*percentile
+
+  for (i in 1:nLines) {
+    # lookup the current DiscrepancyPercent in d in the row matching the chain length
+    currentChainLength=min(groupedDF$ChainLength[i],dr)
+    currentDiscrepancy=groupedDF$DiscrepancyPercent[i]
+    interval[i]=findInterval(currentDiscrepancy, d[currentChainLength,])
+    percentile[i]=interval[i]/C+1/(2*C)
+    interceptForCLModel[i]=t[1,1]+t[1,2]*percentile[i]
+    slopeForCLModel[i]=t[2,1]+t[2,2]*percentile[i]
   }
-  dByPlacementByLength$interval <- interval
-  dByPlacementByLength$percentile <- percentile
-  dByPlacementByLength$intercept <- interceptForCLModel
-  dByPlacementByLength$slope <- slopeForCLModel
-  return(dByPlacementByLength)
+  groupedDF$interval <- interval
+  groupedDF$percentile <- percentile
+  groupedDF$intercept <- interceptForCLModel
+  groupedDF$slope <- slopeForCLModel
+  return(groupedDF)
 }
 
+getSamplePlacementData <- function (rawDF) {
+  filterAndGroupRawData(rawDF,"2015-07-01","2015-07-08")
+}
+
+getTestPlacementData <- function(rawDF) {
+  filterAndGroupRawData(rawDF,"2015-07-09","2015-07-23")
+}
+
+averagePlacmentModel <- function(groupedDF) {
+  placementModelDF <- groupedDF %>%
+    group_by(placementId) %>%
+    select(interceptForCLModel,slopeForCLModel) %>%
+      summarise(interceptForCLModel=mean(interceptForCLModel),
+                slopeForCLModel=mean(slopeForCLModel))
+}
+
+perdictUsingPlacementModel <- function(groupedDF,placementModelDF) {
+  nLines <- nrow(groupedDF)
+  predicted <- vector(length=nLines)
+  for (i in 1:nLines) {
+    currentPId <- groupedDF$placementId[i]
+    currentChainLength <- groupedDF$ChainLength[i]
+    intercept <- placementModelDF$intercept[placementModelDF$placementId==currentPId]
+    if (identical(intercept,numeric(0))) {
+      predicted[i]<-0
+    } else {
+      slope <- placementModelDF$slope[placementModelDF$placementId==currentPId]
+      predicted [i] <- intercept + slope[i]*currentChainLength
+    }
+  }
+
+  groupedDF$predicted <- predicted
+  return(groupedDF)
+}
+
+main <- function() {
+  rawDF <- getRawPlacementData()
+  sampleDF <- getSamplePlacementData(rawDF)
+  discrepancyModel <- getDiscrepancyModel(sampleDF)
+  testDF <- getTestPlacementData(rawDF)
+  predictionDF <- perdictUsingPlacementModel(testDF, discrepancyModel)
+  return(predictionDF)
+}
