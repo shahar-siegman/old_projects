402ff308060a17feeddfea051852e06ba09d4ed6
shahar.siegman <shahar@komoona.com>
2015-09-16 15:06:59 +0300

    1. arrange code 2. add results of actual floor price to graphs
    Former-commit-id: 0595446c57079fa90c4b8ed8119617a7108bb94d

--- IndexSSP/session1.R
+++ IndexSSP/session1.R
@@ -1,12 +1,21 @@
 library(ggplot2)
+library(gridExtra)
+
+simulateSingleFile <- function(fname) {
+  auctions <- loadAuctions(fname)
+  performanceDF <- resultsForRange(auctions,0.01,50)
+  return(performanceDF)
+}
+
 loadAuctions <- function(inputFile=NA) {
   if(identical(inputFile,NA))
     inputFile <- "C:\\Shahar\\Projects\\IndexSSP\\data.txt"
   # Read in the data
   x <- scan(inputFile, what="", sep="\n")
   # Separate elements by one or more whitepace
-  y <- strsplit(x, ",")  # bids (seperated by ;), floorprice, responses
-  y <- lapply(y,`[`,1) # take bids, throw rest
+  z <- strsplit(x, ",")  # fields after split: bids (seperated by ;), floorprice, responses
+  trueFloorPrices <- lapply(z,`[`,2)
+  y <- lapply(z,`[`,1) # take bids, throw rest
   y <- unlist(y)
   y <- strsplit(y,";")  # split to individual bids
   y <- lapply(y,as.numeric)
@@ -17,34 +26,38 @@ loadAuctions <- function(inputFile=NA) {
   recordsToRemove <- is.na(firsts) | firsts==0
   firsts <- firsts[!recordsToRemove]
   seconds <- seconds[!recordsToRemove]
+  trueFloorPrices <- as.numeric(trueFloorPrices[!recordsToRemove])
   seconds[is.na(seconds)] <- 0
-  return(list(firsts,seconds,nAuctions))
+  return(list(firsts,seconds,nAuctions,trueFloorPrices))
 }
 
 resultsForRange <- function(auctions, minFloorPrice, maxFloorPrice) {
-  # result <- data.frame(auctions=numeric(0),floorPrice=numeric(0),wins=numeric(0))
   floorPrices <- sort(unique(unlist(auctions[1:2])))
   if (length(floorPrices)==0) {
-    print ("skipping analysis")
+    print ("No bidding information supplied")
     return(data.frame())
-}
-  floorPrices <- floorPrices[floorPrices>=minFloorPrice & floorPrices <= maxFloorPrice]
-  #minFloorPrice=max(minFloorPrice,0)
-  #maxFloorPrice=min(maxFloorPrice,max(auctions[[1]]))
+  }
   print (maxFloorPrice)
-  if (maxFloorPrice==-Inf)
+  floorPrices <- floorPrices[floorPrices>=minFloorPrice & floorPrices <= maxFloorPrice]
+  if (maxFloorPrice==-Inf) {
+    print("No bids in range")
     return(data.frame())
-  #increment=0.02
-  #floorPrices = seq(minFloorPrice, maxFloorPrice, by=increment)
-  r=length(floorPrices)
-  result <- matrix(nrow=r,ncol=2)
+  }
+  r <- length(floorPrices)
+  result <- matrix(nrow=r,ncol=2,dimnames = list(rep(NA,r),c("wins","revenue")))
   for (i in 1:r) {
     fp <- floorPrices[i]
     result[i,] <- resultByFloorPrice(auctions,fp)
   }
-  df=data.frame(auctions=auctions[[3]],floorPrice=floorPrices,wins=result[,1],revenue=result[,2])
-  df$fill=df$wins/df$auctions
-  df$eCpm=df$revenue/df$wins
+
+  actualResult <- resultByFloorPrice(auctions,auctions[[4]])
+  df <- data.frame(floorPrice=floorPrices,wins=result[,1],revenue=result[,2])
+  df <- rbind(df,c(mean(auctions[[4]]),actualResult))
+  df$floorPriceType <- "simulated"
+  df$floorPriceType[nrow(df)] <- "actual"
+  df$auctions <- auctions[[3]]
+  df$fill=df$wins / df$auctions
+  df$eCpm=df$revenue / df$wins
   return(df)
 }
 
@@ -53,29 +66,10 @@ resultByFloorPrice <- function(auctions,floorPrice) {
   seconds <- auctions[[2]]
   winners <- floorPrice <= firsts
   aboveSeconds <- winners & floorPrice >= seconds
-  revenue <- (floorPrice+0.01)*sum(aboveSeconds) + sum(seconds[winners & !aboveSeconds])
+  revenue <- sum((floorPrice+0.01)*aboveSeconds) + sum(seconds[winners & !aboveSeconds])
   return (c(sum(winners), revenue))
 }
 
-# resultAllFloorPrice <-function(auctions) {
-#   firsts <- unlist(lapply(auctions,`[`,1))
-#   seconds <- unlist(lapply(auctions,`[`,2))
-#   firsts[is.na(firsts)] <- 0
-#   seconds[is.na(seconds)] <- 0
-#
-#   floorprices = seq(maxFloorPrice,0,by=-0.02)
-#   r=length(floorprices)
-#   auctions=length(firsts)
-#   wins=numeric(r)
-#   revenue=numeric(r)
-#
-#   #result <- data.frame(auctions=numeric(0),floorPrice=numeric(0),wins=numeric(0), revenue=numeric(0))
-#   wins=0
-#   for(fp in floorPrices) {
-#
-#   }
-#
-# }
 
 loopFiles <- function() {
   inputdir="placement_bids\\"
@@ -98,20 +92,20 @@ loopResults <- function() {
   flist <- list.files(inputdir, pattern="*.csv");
   for (fname in flist) {
     df <- read.csv(paste(inputdir,fname,sep=""))
-    p1 <- ggplot(data=df, aes(y=wins, x=floorPrice))+geom_line()+geom_point()
-    p2 <- ggplot(data=df, aes(y=eCpm, x=fill))+geom_line()+geom_point()+scale_y_log10()
-    p3 <- ggplot(data=df, aes(y=fill, x=floorPrice))+geom_line()+geom_point()
-    p4 <- ggplot(data=df, aes(y=revenue, x=fill))+geom_line()+geom_point()
+    p1 <- ggplot(data=df, aes(y=wins, x=floorPrice, colour=floorPriceType, size=floorPriceType))+
+      geom_line()+geom_point()+scale_size_manual(values=c(4,0.5))
+    p2 <- ggplot(data=df, aes(y=eCpm, x=fill, colour=floorPriceType, size=floorPriceType))+
+      geom_line()+geom_point()+scale_y_log10()+scale_size_manual(values=c(4,0.5))
+    p3 <- ggplot(data=df, aes(y=fill, x=floorPrice, colour=floorPriceType, size=floorPriceType))+
+      geom_line()+geom_point()+scale_size_manual(values=c(4,0.5))
+    p4 <- ggplot(data=df, aes(y=revenue, x=fill, colour=floorPriceType, size=floorPriceType))+
+      geom_line()+geom_point()+scale_size_manual(values=c(4,0.5))
     fname=paste(outputdir,filename=substr(fname,1,nchar(fname)-4),".png",sep="")
     print(paste("file: ",fname,", rows:",nrow(df)))
+    # output a png file with the four graphs on a 2x2 grid:
     png(fname, width=960, height=960)
     grid.arrange(p1,p2,p3,p4,nrow=2,ncol=2)
     dev.off()
   }
 }
 
-simulateSingleFile <- function(fname) {
-  auctions <- loadAuctions(fname)
-  performanceDF <- resultsForRange(auctions,0.01,50)
-  return(performanceDF)
-}
