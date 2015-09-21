8f3e4b6096de4af6c073e59f18a7ab41b4f2c2dd
shahar.siegman <shahar@komoona.com>
2015-09-21 19:06:52 +0300

    extract floor price, optim goal and optim status in R and build matching histogram
    Former-commit-id: 7e2bb1db2be7172ced6d25a88c7b8bd08dfac39a

--- eCpmTooHigh/session1.R
+++ eCpmTooHigh/session1.R
@@ -1,41 +1,59 @@
 library(dplyr)
+library(ggplot2)
 readInput <- function() {
   rawDF<-read.csv("C:/Shahar/Projects/eCpmTooHigh/performance_with_fp.out.csv", sep=";", as.is=TRUE)
   rawDF$performance_date <- as.Date(rawDF$performance_date)
   return (rawDF)
 }
 
-addFloorPriceColumn <- function (rawDF) {
-  nr <- nrow(rawDF)
-  floorPrice <- numeric(nr)
-  for (i in 1:nr) {
-    floorPrice[i] <- getFloorPriceByDate(rawDF$performance_date[i], rawDF$floor_prices[i], rawDF$floor_change_dates[i])
-  }
-
-  rawDF$floorPrice <- floorPrice
-  #rawDF$ecpmRatio <- rawDF$ecpm/ rawDF$floorPrice
+extractFromChangeColumns <- function(rawDF) {
+  floorPriceVec <- extractFloorPriceByDate(rawDF)
+  isOptimizedVec <- extractIsOptimizedByDate(rawDF)
+  optimGoalVec <- extractOptimizationGoal(rawDF)
   data <- rawDF %>%
     transmute(tagid
               , performance_date
               , impressions
               , fill
               , ecpm
-              , floorPrice
-              , ecpmRatio=ecpm/floorPrice
-              , cost)
+              , floorPrice = floorPriceVec
+              , isOptimized = isOptimizedVec
+              , optimizationGoal = optimGoalVec
+              , ecpmSlack = ecpm-floorPrice
+              , cost
+              , meaningfulFloorPrice = floorPrice > 0.15
+              , meaningfulImpressions = impressions > 2000) %>%
+    filter(!is.na(isOptimized) & !is.na(floorPrice))
   return (data)
 }
 
-getFloorPriceByDate <- function(date, floorPriceList, ChangeDates) {
-  changeDateVec <- as.Date(strsplit(ChangeDates, ",")[[1]])
-  diffDays <- changeDateVec - date
-  interval <- findInterval(0,diffDays)
-  floorPriceVec <- as.numeric(strsplit(floorPriceList,",")[[1]])
-  if (interval>0 && sum(diffDays==0)==0)
-    return (floorPriceVec[interval])
-  else
-    return (-1)
+extractFloorPriceByDate <- function (rawDF) {
+  nr <- nrow(rawDF)
+  floorPriceVec <- numeric(nr)
+  for (i in 1:nr) {
+    floorPriceVec[i] <- getValueByDate(rawDF$performance_date[i], rawDF$floor_prices[i], rawDF$floor_change_dates[i])
+  }
+return (floorPriceVec)
 }
+
+extractIsOptimizedByDate <- function (rawDF) {
+  nr <- nrow(rawDF)
+  isOptimizedVec <- numeric(nr)
+  for (i in 1:nr) {
+    isOptimizedVec[i] <- getValueByDate(rawDF$performance_date[i], rawDF$optimization_in_out[i], rawDF$in_out_date[i])
+  }
+  return (isOptimizedVec)
+}
+
+extractOptimizationGoal <- function (rawDF) {
+  nr <- nrow(rawDF)
+  OptimGoalVec <- character(nr)
+  for (i in 1:nr) {
+    OptimGoalVec[i] <- getValueByDate(rawDF$performance_date[i], rawDF$optimization_goal[i], rawDF$goal_change_date[i], as.character)
+  }
+  return (OptimGoalVec)
+}
+
 
 getValueByDate <- function(date, valueList, changeDates, as.type=as.numeric, initialValue=NA, filterChangeDay=TRUE) {
   changeDateVec <- as.Date(strsplit(changeDates, ",")[[1]])
@@ -51,8 +69,14 @@ getValueByDate <- function(date, valueList, changeDates, as.type=as.numeric, ini
   return (value)
 }
 
-plotRatioDensity <- function(data) {
-  data <- data[data$floorPrice>0,]
-  ggplot()+geom_density(data=data,aes(x=ecpmRatio,weight=cost/sum(cost)))
+plots <- function(data) {
+  data1 <- data[data$meaningfulImpressions,]
+  ggplot()+geom_histogram(data=data1,aes(x=ecpmSlack, y=..count../sum(..count..), fill=meaningfulFloorPrice), binwidth=0.125) #
+
+  data2 <- subset(data,meaningfulImpressions & meaningfulFloorPrice & !is.na(optimizationGoal))
+  ggplot()+geom_histogram(data=data2,aes(x=ecpmSlack, fill=optimizationGoal), binwidth=0.15, alpha=0.5) +
+    xlim(-0.2,1)#
+
+
 }
 
