5c43dac4889ec7072f57b92f0954f3c5bc03151e
shahar.siegman <shahar@komoona.com>
2015-10-22 19:13:21 +0300

    Lost impressions: analysis of lost impression prediction using 14-day moving average
    Former-commit-id: c1348d760f317122654db139efca3b228a7ed9cf

--- LostImpressions/session1.R
+++ LostImpressions/session1.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(outliers)
 library(ggplot2)
 p.critical=0.005
+minLength=14
 loadData <- function() {
   rawDF <- read.csv("lost_imps_by_day.csv")
 }
@@ -38,13 +39,49 @@ chisq.pvalue <- function(vec) {
   return(vec)
 }
 
-
-mainAnalysis <- function(rawDF) {
-  periodLength <- 14
+filterLostOutliers <- function(rawDF) {
   rawDF$rel_lost <- multiOutlierFilter(rawDF$placement_id,rawDF$rel_lost)
+  return(rawDF)
+}
+
+Analysis.BoxPlot <- function(rawDF) {
+  periodLength <- 14
+  rawDF <- filterLostOutliers(rawDF)
   rawDF$periodNum <- as.factor(floor(as.numeric(difftime(as.Date(rawDF$date,format="%m/%d/%Y"),as.Date("2015-07-01"),units="days") / periodLength)))
   rawDF <- rawDF %>% filter(placement_id!= "9d5b3ef95e64dc6b252cd1d50e2a579c" & placement_id!= "bab3a2b6c97481906df2ff0051906382" )
   stats <- rawDF %>% group_by(placement_id, periodNum) %>% summarise(m=mean(rel_lost, na.rm=TRUE), s= sd(rel_lost, na.rm=TRUE))
-  g <- ggplot(rawDF) + geom_boxplot(aes(x=periodNum, y=rel_lost)) + facet_wrap(~placement_id) + coord_cartesian(ylim = c(-0.1, 0.5))
+  g <- ggplot(rawDF) + geom_boxplot(aes(x=periodNum, y=rel_lost)) + facet_wrap(~placement_id) + coord_cartesian(ylim = c(-0, 0.2))
   print(g)
 }
+
+Analysis.Zscore <- function(rawDF) {
+  rawDF <- rawDF %>% arrange(placement_id,as.Date(date,format="%m/%d/%Y"))
+  rawDF <- filterLostOutliers(rawDF)
+  rawDF$zScore <- zScore(rawDF$placement_id,rawDF$rel_lost)
+  rawDF$date <- as.Date(rawDF$date, format="%m/%d/%Y")
+  g1 <- ggplot(rawDF) + geom_line(aes(x=date,y=zScore)) + facet_wrap(~placement_id) + coord_cartesian(ylim = c(-2, 2))
+  g2 <- ggplot(rawDF) + geom_density(aes(x=zScore), fill="blue") + facet_wrap(~placement_id) + coord_cartesian(xlim = c(-5, 5))
+  print(g2)
+}
+
+zScore <- function(groupIdentifier,values) {
+
+  subscripts <- tapply(values, groupIdentifier, NULL)
+  reconstructed <- numeric(length(subscripts))
+  for (i in 1:max(subscripts)) {
+    vec <- values[subscripts==i]
+    len <- length(vec)
+    z <- numeric(len)
+    z <- z*NA
+    if (len > minLength) {
+      for (d in (minLength+1):len) {
+        m <- mean(vec[(d-minLength):(d-1)],na.rm=TRUE)
+        t <- sd(vec[(d-minLength):(d-1)],na.rm=TRUE)
+        z[d]=(vec[d]-m)/t
+      }
+    }
+    reconstructed[subscripts==i] <- z
+  }
+  return (reconstructed)
+}
+
