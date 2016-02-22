4f130cb2db065614ad08f564f7736b6c14849de6
shahar.siegman <shahar@komoona.com>
2016-02-22 12:19:25 +0200

    tagdata directory
    Former-commit-id: 109085468cba05b4f8ffc74bb000c733548d594b

--- tagData/run_parallel.bat
+++ tagData/run_parallel.bat
@@ -1,2 +1,2 @@
-call ppx2 -L 1 -P 8 run_one.bat 2015-09-01 {}< C:\Shahar\Projects\tagData\input_placements.csv
+call ppx2 -L 1 -P 8 run_one.bat 2016-01-01 {}< C:\Shahar\Projects\tagData\input_placements.csv
 call run_join
