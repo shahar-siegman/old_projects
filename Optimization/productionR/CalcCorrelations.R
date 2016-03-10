f5bd61efaab338ffbcbdb114973d8de78ff6c311
shahar.siegman <shahar@komoona.com>
2016-03-10 16:00:05 +0200

    continue correl based fill model. model working for both 1st and 2nd order, exploring results
    Former-commit-id: eeade3f337e8eb3d1191d0b8731de82d020de3d4

--- Optimization/productionR/CalcCorrelations.R
+++ Optimization/productionR/CalcCorrelations.R
@@ -1,59 +1,25 @@
 source('../../libraries.R')
 
-testCalcCorrel <- function () {
-  inputs <- loadAllInputs()
+runCalcCorrel <- function (order=1, inputPath= 'data') {
+  inputs <- loadAllInputs(inputPath)
   integrateDate <- inputs %>% group_by(network,FloorPrice,pfx,ordinal) %>%
     summarise(impressions = sum(served/Fill),served=sum(served)) %>%
     mutate(Fill = served/ impressions) %>% ungroup()
-  fills <- loadAllFillTables()
+  tabulatedModels <- loadAllFillTables(inputPath)
 
-  joinedFills <- getFillsForAllPreChains(integrateDate,2)
-  joinedFills <- getModelOrdinalZeroFillForCurrentTag(joinedFills,fills)
-
-  scoreDF <- calcOrder2Coeffs(joinedFills)
+  return(calcCorrel(integrateDate,tabulatedModels, order))
 }
 
-runCalcCorrel <- function ()
-{
-  inputs <- loadAllInputs()
-  integrateDate <- inputs %>% group_by(network,FloorPrice,pfx,ordinal) %>%
-    summarise(impressions = sum(served/Fill),served=sum(served)) %>%
-    mutate(Fill = served/ impressions) %>% ungroup()
-  integrateDate <- integrateDate %>% prefixFills(2)
-  fills <- loadAllFillTables()
-  return(calcCorrel(integrateDate, fills, 2))
-}
 
 calcCorrel <- function (inputs, tabulatedModels, order) {
-  orderRows <- inputs[inputs$ordinal==order,]
-  tagsWithOrder0Fill <- adply(orderRows, .margins=1, .fun=matchOrderZeroFill, .id= "id", tabulatedModels)
-  orderTagsObjectiveDF <- ddply(tagsWithOrder0Fill, .variables="groupingTag", .fun=calcOrderCoeffs, order)
-  best <-  optim(c(0,0),fn=score, gr=NULL, orderTagsObjectiveDF)
-    #optimLoop(orderTagsObjectiveDF)
+  joinedFills <- getFillsForAllPreChains(inputs,order)
+  joinedFills <- getModelOrdinalZeroFillForCurrentTag(joinedFills,tabulatedModels)
+
+  scoreDF <- calcOrderCoeffs(joinedFills, order)
+
+  best <-  optim(c(0,0),fn=score, gr=NULL, scoreDF)
+
   return(best)
-}
-
-
-matchOrderZeroFill <- function(inputRow, tabulatedModels)
-{
-  # assumes only ordinal-2 rows in input
-  currentTag <- paste0(inputRow$network,inputRow$FloorPrice)
-  chain <- paste0(inputRow$pfx,":",currentTag)
-  chainTags <- parseChain(chain) %>% rename(original_ordinal=ordinal)
-
-  chainTags$ordinal="0"
-  chainTagsWithFills <- left_join(chainTags %>%
-                                    mutate(network=as.character(network)),
-                                  tabulatedModels  %>%
-                                    select(ordinal, network, FloorPrice, Fill=value),
-                                  by=c("network","FloorPrice","ordinal"))
-  chainTagsWithFills$FillSource <- "model"
-  currentTagRow <- inputRow %>%
-    select(original_ordinal=ordinal,network,FloorPrice, Fill)
-  currentTagRow$FillSource <- "actual"
-  result <- rbind(chainTagsWithFills %>% select(-ordinal), currentTagRow)
-  result$groupingTag <- currentTag
-  return(result)
 }
 
 calcOrderCoeffs <- function(chainDF, order)
@@ -66,18 +32,21 @@ calcOrderCoeffs <- function(chainDF, order)
 
 calcOrder1Coeffs <- function(chainDF)
 {
-  net <- chainDF$network
-  fill <- 1 - chainDF$Fill
-  corr12Type <- ifelse(net[1]==net[2],"s","d")
-  one <- fill[2]-fill[3]
-  denom <- fill[1]
-  corr12coeff <- sqrt((1-fill[1])*fill[1]*(1-fill[3])*fill[3])/denom
+  sigXsigY <- function(x,y) sqrt(x*y*(1-x)*(1-y))
 
-  s=0; d=0; ss=0; sd=0; dd=0
-  if (corr12Type=="s")
-    s = s + corr12coeff
-  else
-    d = d + corr12coeff
+  passrate1 <- 1-chainDF$Fill.0
+  passrate2 <- 1-chainDF$Fill.1
+  passrateBase <- 1-chainDF$Fill.Model
+  net1 <- substr(chainDF$chain.0,1,1)
+  net2 <- chainDF$chain.1 %>% word(2,2,":") %>% substr(1,1)
+
+  corr12Type <- ifelse(net1==net2,"s","d")
+  one <- passrateBase - passrate2
+
+  corr12coeff <- sigXsigY(passrate1,passrateBase) / passrate1
+  s=ifelse(corr12Type=="s",corr12coeff,0)
+  d=ifelse(corr12Type=="d",corr12coeff,0)
+
 
   result <- data.frame("one"=one, "s"=s,"d"=d)
   return(result)
@@ -85,7 +54,6 @@ calcOrder1Coeffs <- function(chainDF)
 
 calcOrder2Coeffs <- function(chainDF)
 {
-  net <- chainDF$network
   passrate1 <- 1-chainDF$Fill.0
   passrate2 <- 1-chainDF$Fill.1
   passrate3 <- 1-chainDF$Fill.2
@@ -102,19 +70,8 @@ calcOrder2Coeffs <- function(chainDF)
   corr13coeff <- sigXsigY(passrate1,passrateBase) / passrate1
   corr23coeff <- sigXsigY(passrate2,passrate3) / (passrate1*passrate2)
 
-  s=0
-  d=0
-
-  if (corr13Type=="s")
-    s = s + corr13coeff
-  else
-    d = d + corr13coeff
-
-  if (corr23Type=="s")
-    s = s + corr23coeff
-  else
-    d = d + corr23coeff
-
+  s=ifelse(corr13Type=="s",corr13coeff,0) + ifelse(corr23Type=="s",corr23coeff,0)
+  d=ifelse(corr13Type=="d",corr13coeff,0) + ifelse(corr23Type=="d",corr23coeff,0)
 
   result <- data.frame("one"=one, "s"=s,"d"=d)
 
--- Optimization/productionR/FileRead.R
+++ Optimization/productionR/FileRead.R
@@ -1,25 +1,25 @@
 source('../../libraries.R')
 library('stringr')
-inputFilePath = 'data'
 
-loadAllInputs <- function() {
+
+loadAllInputs <- function(inputFilePath = 'data') {
   files <- list()
   files$names <- list.files(path = inputFilePath, pattern="df_data_[A-Za-z]+_[0-9]+.csv")
   files$networks <- str_extract(files$names, "[A-Z][A-Za-z]+")
   files$ordinals <- str_extract(files$names, "[0-9]+")
-  return(loadFiles(files))
+  return(loadFiles(files, inputFilePath))
 }
 
-loadAllFillTables <- function ()
+loadAllFillTables <- function (inputFilePath = 'data')
 {
   files <-list()
   files$names <- list.files(path = inputFilePath, pattern="Fill_[A-Za-z]+_[0-9]\\.csv")
   files$networks <- str_extract(substr(files$names,6,100), "[A-Z][A-Za-z]+")
   files$ordinals <- str_extract(substr(files$names,6,100), "[0-9]+")
-  return(loadFiles(files))
+  return(loadFiles(files, inputFilePath))
 }
 
-loadFiles <- function(files)
+loadFiles <- function(files, inputFilePath)
 {
   networks <- networkDict()
   nfiles <- length(files$names)
@@ -39,5 +39,5 @@ loadFiles <- function(files)
 
 
 networkDict <- function () {
-  return (c("Aol"="o","Index"="j","OpenX"="e","Pubmatic"="p"))
+  return (c("Aol"="o","Index"="j","OpenX"="e","Pubmatic"="p", "PulsePoint"="t"))
 }
