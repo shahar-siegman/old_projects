259c70d7fd91cef57e646dce36f45f611a9bd630
shahar.siegman <shahar@komoona.com>
2016-03-20 13:43:23 +0200

    served prediction model based on same-day redshift data
    Former-commit-id: 3662b6f440aef9ca931602a3fe88ef1ac75db5f4

--- RedShift/PublisherReport/session1.R
+++ RedShift/PublisherReport/session1.R
@@ -11,12 +11,14 @@ preprocess <- function(df)
   df3 <- df3 %>% mutate(tag=ifelse(code=="",served_tag_network,code))
   df3 <- df3 %>% mutate(date=ifelse(date=="", date_1,date))
   df3 <- df3 %>%  filter(tag %in% c("e","o","p","t","j","x","z") , as.Date(date) <='2016-03-13')
+  df3 <- df3 %>% select(plcmnt_joined, date, tag, cnt, served, impressions) %>%
+    rename(placement_id=plcmnt_joined) %>% mutate(code=tag)
   return(df3)
 }
 
-summarisePlacementNetwork <- function(df)
+summarisePlacementNetwork <- function()
 {
-  df4 <- df3 %>% preprocess() %>% group_by(placement_id,tag) %>% summarise(cnt=sum(cnt), served=sum(served))
+  df4 <- getData() %>% preprocess() %>% group_by(placement_id,tag) %>% summarise(cnt=sum(cnt), served=sum(served))
   df4$cnt[is.na(df4$cnt)]=0
   df4$served[is.na(df4$served)]=0
   df4 <- df4 %>% filter(cnt + served >= 500)
@@ -26,19 +28,51 @@ summarisePlacementNetwork <- function(df)
   return(df4)
 }
 
-analysis1 <- function(df)
+analysis1 <- function()
 {
-  df <- getData()
-  df1 <- summarisePlacementNetwork(df)
+  df1 <- summarisePlacementNetwork()
   ggplot(df1) + geom_density(aes(x=rs_bias, y=..scaled..)) + facet_grid(~tag, scale='free')
 }
 
-analysis2 <- function(df)
+calcRsBias <- function()
 {
   df <- getData() %>% preprocess()
-  df1 <- summarisePlacementNetwork(df) %>% select(placement_id, tag)
+  df1 <- summarisePlacementNetwork() %>% select(placement_id, tag)
   df2 <- inner_join(df, df1, by=c("placement_id","tag"))
   df2 <- df2 %>% mutate(rs_bias = ifelse(cnt<served,cnt/served-1, 1-served/cnt))
-  ggplot(df2) + geom_path(aes(x=date, y=rs_bias, group=tag, colour=tag)) +
-    facet_wrap( ~ plcmnt_joined) + coord_cartesian(ylim=c(-0.3,0.3))
+  return(df2)
+}
+
+analysis2 <- function()
+{
+  df2 <- calcRsBias()
+  ggplot(df2) +  geom_abline(slope=0, intercept=0, colour="gray") +
+    geom_path(aes(x=date, y=rs_bias, group=tag, colour=tag)) +
+    facet_wrap( ~ placement_id) + coord_cartesian(ylim=c(-0.3,0.3))
+}
+
+analysis3 <- function()
+{
+  df2 <- calcRsBias()
+  df2 <- df2 %>% arrange(date) %>% group_by(placement_id,tag) %>% mutate(lag_served=lag(served)) %>% ungroup()
+  df2 <- df2 %>% mutate(lag_bias = ifelse(lag_served<served,lag_served/served-1, 1-served/lag_served))
+  ggplot(df2) +  geom_abline(slope=0, intercept=0, colour="gray") +
+    geom_path(aes(x=date, y=lag_bias, group=tag, colour=tag)) +
+    facet_wrap( ~ placement_id) + coord_cartesian(ylim=c(-0.3,0.3))
+}
+
+analysis4 <- function(Print=F, w=4)
+{
+  df2 <- calcRsBias()
+  df2 <- df2 %>% arrange(date) %>% group_by(placement_id,tag) %>%  mutate(lag_rs_bias=lag(rs_bias)) %>% ungroup()
+  df3 <- df2 %>% arrange(placement_id, code, date)
+  df3 <- df3 %>% mutate(bias_pred=rollapply(data = lag_rs_bias, width = w, mean, na.rm=T, partial=T, fill=NA, align="right"))
+  df3 <- df3 %>% mutate(pred_served = ifelse(bias_pred>0,(1-bias_pred)*cnt,cnt/(bias_pred+1)))
+  df3 <- df3 %>% mutate(pred_bias = ifelse(pred_served < served, pred_served/served-1, 1-served/pred_served))
+  p <-  ggplot(df3) +  geom_abline(slope=0, intercept=0, colour="gray") +
+    geom_path(aes(x=date, y=pred_bias, group=tag, colour=tag)) +
+    facet_wrap( ~ placement_id) #+ coord_cartesian(ylim=c(-0.3,0.3))
+  if (Print)
+    print(p)
+  return(df3)
 }
