320e8f3889b5199caabcc037c3cd6ca54a653eec
shahar.siegman <shahar@komoona.com>
2016-03-21 17:32:56 +0200

    more work on publisher report + arrange video queries
    Former-commit-id: ccb3fd6c4eb08a037f3a9e4649e0309e3e455abe

---
+++ RedShift/PublisherReport/session2.R
@@ -0,0 +1,87 @@
+source('../../libraries.R')
+library("L1pack")
+getListTagData <- function()
+{
+  read.csv('list2_placements_tag_performance.csv', stringsAsFactors = F)
+}
+
+preprocess2 <- function(df)
+{
+  df1 <- df %>% mutate(ecpm = 1000*income/served,
+                      code=substr(tag_name,1,1),
+                      week=as.factor(floor(as.numeric(as.Date(date_joined)-as.Date('2016-02-10'))/7)))
+
+  df1 <- df1 %>% select(placement_id, week, date_joined, floor_price, served, income, tag_name, code, ecpm)
+  df1 <- df1 %>% filter(code !="", served>500)
+  return(df1)
+}
+
+analysis6 <- function(df)
+{
+  # df is after preprocess2
+  # plot ecpm vs. floor price per network
+  ggplot(df) + geom_point(aes(x=floor_price,y=ecpm,colour=week)) +
+    geom_abline(slope=1,intercept=0,colour="grey") +
+    geom_abline(slope=0.85,intercept=0,colour="blue") +
+    facet_wrap(~code)
+}
+
+analysis7 <- function(df)
+{
+  # df is after preprocess2
+  # see if yesterday's ecpm is a good prediction
+  #df <- df %>% filter(ecpm>1.05*floor_price)
+  df <- df %>% arrange(placement_id, floor_price, tag_name, date_joined) %>%
+    group_by(placement_id, floor_price, tag_name) %>%
+    mutate(ecpm_lag=lag(ecpm)) %>% ungroup()
+  ggplot(df) +  geom_point(aes(x=ecpm_lag,y=ecpm,colour=week)) +
+    geom_abline(slope=1,intercept=0,colour="grey") + facet_wrap(~code)
+}
+
+fitEcpmByNetwork <- function()
+{
+  df <- getListTagData()
+  df <- preprocess2(df)
+  networks <- "ejoptx"
+  nnet <- str_length(networks)
+  netmodels <- data.frame()
+  for (i in 1:nnet) {
+    network <- substr(networks,i,i)
+    data <- df %>% filter(code==network)
+    currentLm <- lm(ecpm ~ floor_price + 0, data)
+    currentAd <- lad(ecpm ~ floor_price + 0, data)
+    # find other key statistics: mode and median
+    currentMode <- Mode(data$ecpm/data$floor_price)
+    currentMedian <- median(data$ecpm/data$floor_price)
+    netmodels <- rbind(netmodels,data.frame(
+      network=network,
+      coeff = currentLm$coefficients[1],
+      ad = currentAd$coefficients[1],
+      mode = currentMode,
+      median=currentMedian))
+  }
+  rownames(netmodels) <- NULL
+  return(netmodels)
+
+}
+
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+
+ecpmFloorPriceCoeffs <- function()
+{
+  lines <- "Network, coeff
+         e, 1.1813321
+         j, 1.0135180
+         o, 0.8985693
+         p, 0.9902152
+         t, 1.0000054
+         x, 1.1137289"
+  con <- lines %>% textConnection()
+  data <- con %>% read.csv()
+  con %>% close()
+  return(data)
+
+}
