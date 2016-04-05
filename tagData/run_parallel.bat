93671d70b84d91871fc3fcd967c50c20c9271597
shahar.siegman <shahar@komoona.com>
2016-04-05 13:51:09 +0300

    small changes
    Former-commit-id: 8dbf07fbead93cd7b75184edeb7e22ca96d44b3d

--- tagData/run_parallel.bat
+++ tagData/run_parallel.bat
@@ -1,2 +1,2 @@
-call ppx2 -L 1 -P 8 run_one.bat "2016-01-27" {}< C:\Shahar\Projects\tagData\input_placements.csv
+call ppx2 -L 1 -P 8 run_one.bat "2016-03-01" {}< C:\Shahar\Projects\tagData\input_placements.csv
 call run_join
