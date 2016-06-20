d1a6cbc1481f7f9a3355069277bdad8a46ad5402
shahar.siegman <shahar@komoona.com>
2016-06-20 13:15:34 +0300

    pubmatic tag type (text) fill investigation
    Former-commit-id: b1d356738a68d7076ae8325ace11d31e5f9a061c

---
+++ sandbox/pubmatic tag type issue/session1.R
@@ -0,0 +1,23 @@
+source('../../libraries.R')
+
+#a <- read.csv('performance_with_good_bad.csv',stringsAsFactors = F)
+
+b <- a %>% filter(tag_type %in% c('good','bad'),!is.na(ordinal), impressions>100) %>%
+  mutate(floor_price_bin=round(floor_price*2.5)/2.5,
+         ordinal=as.factor(ordinal),
+         tag_type=as.factor(tag_type)) %>%
+  group_by(placement_id, tag_type, ordinal, floor_price_bin) %>%
+  summarise(impressions=sum(impressions), served=sum(served), income=sum(income)) %>%
+  mutate(fill=served/impressions, ecpm=1000*income/served, rcpm=1000*income/impressions)
+
+d <- b %>% dcast(placement_id + ordinal + floor_price_bin ~ tag_type,value.var="fill") %>%
+  filter(!is.na(bad),!is.na(good)) %>%
+  mutate(win=ifelse(good>bad,1,0))
+
+g <- b %>% group_by(floor_price_bin, tag_type)  %>% summarise(cnt=n(),served=sum(served),imps=sum(impressions), income=sum(income)) %>%
+  mutate(ecpm=1000*income/served, fill=served/imps, rcpm=1000*income/imps)
+p1 <- ggplot(b) + geom_point(aes(x=placement_id,y=fill,colour=tag_type)) + facet_grid(floor_price_bin~ordinal) #+coord_cartesian(ylim=c(0,0.2))
+p2 <- ggplot(b) + geom_point(aes(x=placement_id,y=ecpm,colour=tag_type)) + facet_grid(floor_price_bin~ordinal)
+print(c(sum(d$win), sum(d$win)/nrow(d), mean(d$good), mean(d$bad)))
+
+p3 <- ggplot(b) + geom_density(aes(x=rcpm,colour=tag_type))+ facet_wrap(~floor_price_bin)
