182d9b495bc97adaf506ceb740cde338ee01d031
shahar.siegman <shahar@komoona.com>
2016-07-18 18:05:14 +0300

    uid recurrence and first kbidder script
    Former-commit-id: d4a3746f70a5e862d7c66c422a675b3a19b6e12d

---
+++ RedShift/UidReccurence/session1.R
@@ -0,0 +1,13 @@
+source('../../libraries.R')
+
+a <- read.csv('50_cookies_july_grouped.csv',stringsAsFactors = F)
+
+a1 <- a %>% filter(uid=='e56b13c36ea3e94c232e87a2ec5e9e4e')
+
+
+asDateTime <- function(a,cols) # convert the columns whose names are in cols to dateTime
+{
+  for (i in 1:length(cols))
+    a[cols[i]] <- as.Date(a[cols[i]],'%Y/%m/%d %H:%M:%S')
+  return(a)
+}
---
+++ kbidder/extractFromLogs.js
@@ -0,0 +1,84 @@
+const fs = require('fs');
+var readline = require('readline');
+var csvWriter = require('csv-write-stream');
+var writer = csvWriter();
+var inputFiles = ['kbidder_10_11.txt', 'kbidder_11_12.txt', 'kbidder_12_13.txt', 'kbidder_13_14.txt', 'kbidder_14_15.txt', 'kbidder_15_16.txt'];
+//
+function loadData(dataFile) {
+    var outFile = inputFile.split('.')[0]+'_parsed.csv';
+    writer.pipe(fs.createWriteStream(outFile));
+    var lineReader = readline.createInterface({
+        input: fs.createReadStream(dataFile)});
+    
+    lineReader
+    .on("line", function(row){
+        //data.moment = moment(data.date, "YYYY-MM-DD");
+        handleLine(row);
+    })
+    .on("end", function(){
+        console.log("done");
+        writer.end();
+    });
+
+}
+
+function handleLine(row) {
+    row =row.toString();
+    var rowParts = row.split('-->');
+    var timeAndTypeParts = rowParts[0].split(':');
+    //console.log(rowParts[1].trim());
+    var record = JSON.parse(rowParts[1].trim());
+    var basicProperties = {
+        time: (timeAndTypeParts[1]+':'+timeAndTypeParts[2]+':'+timeAndTypeParts[3]).trim(),
+        log_type: timeAndTypeParts[0].trim(),
+        tagid: record.tagid,
+        cb: record.cb,
+    }
+    if (basicProperties.log_type=="tag" && record.parent)
+        return; // skip row if it was mistakenly matched
+    var extendedProperties;
+    if (basicProperties.log_type=="placement" && record.hdbdId) // there is hdbd data
+    { 
+        var hb=JSON.parse(record.hb);
+        var bidders = {};
+        for (var i=0; i< hb.length; i++ ) {
+            if(hb[i].code)
+                bidders[hb[i].code]=hb[i].cpm;
+        }
+        extendedProperties = {
+            l1_bid: bidders && bidders.l1 ? bidders.l1 : null,
+            o2_bid: bidders && bidders.o2 ? bidders.o2 : null,
+            p5_bid: bidders && bidders.p5 ? bidders.p5 : null,
+            kbwt: record.kbwt,
+            served_tag: record.o.split('|').pop(),
+            cpm: record.cpm,
+            wb: record.wb,
+            sent_bid: record.sb
+        }
+    }
+    else 
+    {
+        extendedProperties = {
+            l1_bid: null,
+            o2_bid: null,
+            p5_bid: null,
+            kbwt: null,
+            served_tag: record.o? record.o.split('|').pop() : null ,
+            cpm: record.cpm,
+            wb: null,
+            sent_bid: null
+        }
+    }
+
+    for (attr in extendedProperties) basicProperties[attr]=extendedProperties[attr];
+
+    writer.write(basicProperties);
+    //console.log('next!');
+}
+
+for (var i=1; i< inputFiles.length; i++) {
+    consolse.log('processing ' + inputFiles[i]);
+    loadData(inputFiles[i]);
+    console.log('done with ' + inputFiles[i]);
+}
+    
