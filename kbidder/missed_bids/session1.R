20df9ac58254941e74e059a6aafa063ae4df59d8
shahar.siegman <shahar@komoona.com>
2016-11-02 14:39:31 +0200

    missed bids R code first version

--- kbidder/missed_bids/session1.R
+++ kbidder/missed_bids/session1.R
@@ -1,6 +1,69 @@
 source('../../libraries.R')
 
-a <- read.csv('timing_analysis_Oct4.csv',stringsAsFactors = F)
-b <- read.csv('placement_estimated_delay.csv',stringsAsFactors = F)
+# a <- read.csv('timing_analysis_Oct4.csv',stringsAsFactors = F)
+# b <- read.csv('placement_estimated_delay.csv',stringsAsFactors = F)
+#
+# a1 <- a %>% inner_join(b,by='placement_id')
 
-a1 <- a %>% inner_join(b,by='placement_id')
+a <- read.csv('all_kbidder_bids_oct30-0-5.csv', stringsAsFactors = F)
+b <- read.csv('all_kbidder_placement_names.csv', stringsAsFactors = F)
+
+a1 <- inner_join(a,b, by=c('placement_id'='layoutid'))
+
+a2 <- a1 %>%
+  mutate(improvement_until_expiry=
+           ifelse(max_bid_before_suggested < 0.2 & max_bid_suggested_to_timeout > 0.2,max_bid_suggested_to_timeout,
+                  ifelse(max_bid_before_suggested>max_bid_suggested_to_timeout,0,
+                         max_bid_suggested_to_timeout-max_bid_before_suggested)),
+         max_bid_until_timeout=pmax(max_bid_before_suggested,max_bid_suggested_to_timeout),
+         improvement_after_expiry=
+           ifelse(max_bid_until_timeout < 0.2 & max_bid_after_timeout > 0.2,max_bid_after_timeout,
+                  ifelse(max_bid_until_timeout > max_bid_after_timeout,0,
+                         max_bid_after_timeout-max_bid_until_timeout))
+  )
+
+a3 <-a2 %>% mutate(improvement_until_expiry_bin=pmin(floor(improvement_until_expiry/0.1)*0.1,10),
+                   improvement_after_expiry_bin=pmin(floor(improvement_after_expiry/0.1)*0.1,10))
+
+a4.until <- a3 %>%
+  group_by(placement_id) %>% mutate(n_imps=n()) %>%
+  group_by(placement_id,name,improvement_until_expiry_bin,n_imps)  %>% summarise(count=n()) %>%
+  mutate(relative_count=count/n_imps)
+
+a4.after <- a3 %>%
+  group_by(placement_id) %>% mutate(n_imps=n()) %>%
+  group_by(placement_id,name,improvement_after_expiry_bin,n_imps)  %>% summarise(count=n()) %>%
+  mutate(relative_count=count/n_imps)
+
+
+plot.bid.distrib <- function(placement_num) {
+  placements <- unique(a2$placement_id)
+  a3 <- a2 %>% filter(placement_id==placements[placement_num])
+  print(a2$name[1])
+  ggplot(a3)+geom_histogram(aes(x=improvement_until_expiry),fill="blue",alpha=0.4,binwidth=0.1)+
+    geom_histogram(aes(x=improvement_after_expiry),fill="green",alpha=0.2,binwidth=0.1) +
+    #geom_density(aes(x=max_bid_after_timeout,y=..density..),colour="black")+
+    coord_cartesian(xlim=c(0,1))
+}
+
+plot.until.bins <- function(placement_num) {
+  placements <- unique(a4.until$placement_id)
+  a5 <- a4.until %>% filter(placement_id==placements[placement_num], improvement_until_expiry_bin>0)
+  print(a5$name[1])
+  ggplot(a5)+geom_bar(aes(x=improvement_until_expiry_bin,y=relative_count),stat="identity",fill="blue")+
+    scale_y_continuous(labels=percent)
+}
+
+plot.after.bins <- function(placement_num) {
+  placements <- unique(a4.after$placement_id)
+  a5 <- a4.after %>% filter(placement_id==placements[placement_num], improvement_after_expiry_bin>0)
+  print(a5$name[1])
+  ggplot(a5)+geom_bar(aes(x=improvement_after_expiry_bin,y=relative_count),stat="identity",fill="green")+
+    scale_y_continuous(labels=percent)
+}
+
+# bid_improvement_to_bins <- function(placement_num) {
+#   placements <- unique(a2$placement_id)
+#   a3 <- a2 %>% filter(placement_id==placements[placement_num])
+#
+# }
