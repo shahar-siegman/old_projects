e1d1553f1f4bf9df0e11d90da5fbd9d3b190c796
shahar.siegman <shahar@komoona.com>
2017-05-14 10:04:13 +0300

    1. moving user-filtering related js code 2. refactor analysis code to be per-placement

--- kbidder/Node/user_filtering/H-sovrn_low_bid_high_win_placements.js
+++ kbidder/Node/user_filtering/H-sovrn_low_bid_high_win_placements.js
@@ -30,19 +30,7 @@ var h = fs.createReadStream('./data/cookie_sample5_c.csv', 'utf8')
         data.ord = +data.ord
         this.queue(data)
     }))
-/*
-var i = h
-    .pipe(gb.groupBy(['placement_id', 'uid'], false, {
-        session_length: gb.max('ord'),
-        sovrn_requests: gb.sum('sovrn_requests'),
-        sovrn_responses: gb.sum('sovrn_responses'),
-        sovrn_bids: gb.sum('sovrn_bids'),
-        sovrn_wins: gb.sum('sovrn_wins')
-    }))
-    .pipe(filter.obj(data => data.session_length > 3))
-    .pipe(fastCsv.createWriteStream({ headers: true }))
-    .pipe(fs.createWriteStream('./data/cookie_sample5_i.csv', 'utf8')).on('finish', function () { console.log('pc sample i - done. parse errors: ' + parseErrors) })
-*/
+
 var j = h
     .pipe(through(function (data) {
         data.sovrn_revenue = data.kb_code[0] == 'S' ? data.cpm : 0
@@ -50,84 +38,78 @@ var j = h
     }))
     .pipe(gb.groupByHyb(['placement_id', 'uid'], false, {
         impressions_in_session: gb.max('ord'),
-        requests_in_session: gb.sum('sovrn_requests'),
+        wins_in_session: gb.sum('sovrn_wins'),
         bids_in_session: gb.sum('sovrn_bids'),
         revenue_in_session: gb.sum('sovrn_revenue'),
     }))
     .pipe(gb.groupBy(['placement_id', 'uid'], true, {
-        session_cum_requests: gb.sum('sovrn_requests'),
+        session_cum_wins: gb.sum('sovrn_wins'),
         session_cum_bids: gb.sum('sovrn_bids'),
         session_cum_revenue: gb.sum('sovrn_revenue'),
     }))
-    .pipe(sort(comp(['ord', 'session_cum_bids'])))
-    .pipe(gb.groupBy(['ord', 'session_cum_bids'], false, {
+    .pipe(sort(comp(['placement_id', 'ord', 'session_cum_bids'])))
+    .pipe(gb.groupBy(['placement_id', 'ord', 'session_cum_bids'], false, {
         impressions: gb.count(),
-        requests: gb.sum('sovrn_requests'),
+        wins: gb.sum('sovrn_wins'),
         bids: gb.sum('sovrn_bids'),
         revenue: gb.sum('sovrn_revenue'),
         impressions_upto_here: gb.sum('ord'),
-        requests_upto_here: gb.sum('session_cum_requests'),
+        wins_upto_here: gb.sum('session_cum_wins'),
         bids_upto_here: gb.sum('session_cum_bids'),
         revenue_upto_here: gb.sum('session_cum_revenue'),
         impressions_all_relevant_sessions: gb.sum('impressions_in_session'),
-        requests_all_relevant_sessions: gb.sum('requests_in_session'),
+        wins_all_relevant_sessions: gb.sum('wins_in_session'),
         bids_all_relevant_sessions: gb.sum('bids_in_session'),
         revenue_all_relevant_sessions: gb.sum('revenue_in_session')
     }))
-    .pipe(gb.groupBy(['ord'], true, {
+    .pipe(gb.groupBy(['placement_id', 'ord'], true, {
         impressions_upto_here_cum: gb.sum('impressions_upto_here'),
-        requests_upto_here_cum: gb.sum('requests_upto_here'),
+        wins_upto_here_cum: gb.sum('wins_upto_here'),
         bids_upto_here_cum: gb.sum('bids_upto_here'),
         revenue_upto_here_cum: gb.sum('revenue_upto_here'),
         impressions_all_cum: gb.sum('impressions_all_relevant_sessions'),
-        requests_all_cum: gb.sum('requests_all_relevant_sessions'),
+        wins_all_cum: gb.sum('wins_all_relevant_sessions'),
         bids_all_cum: gb.sum('bids_all_relevant_sessions'),
         revenue_all_cum: gb.sum('revenue_all_relevant_sessions')
     }))
-    .pipe(gb.groupByHyb([], true, {
+    .pipe(gb.groupByHyb(['placement_id'], true, {
         impressions_total: gb.sum('impressions'),
-        requests_total: gb.sum('requests'),
+        wins_total: gb.sum('wins'),
         bids_total: gb.sum('bids'),
         revenue_total: gb.sum('revenue'),
     }))
-    .pipe(through(function(data) {
-        data.impressions_blocked = data.impressions_all_cum - data.impressions_upto_here_cum
-        data.requests_blocked = data.requests_all_cum - data.requests_upto_here_cum
-        data.bids_blocked = data.bids_all_cum - data.bids_upto_here_cum 
-        data.revenue_blocked = data.revenue_all_cum - data.revenue_upto_here_cum
+    .pipe(filter.obj(data =>
+        data.impressions_total > 10 &&
+        data.bids_total > 5 &&
+        data.revenue_total > 0.1 &&
+        data.ord <= 100
+        ))
+    .pipe(through(function (data) {
+        var res = {
+            placement_id: data.placement_id,
+            ord: data.ord,
+            session_cum_bids: data.session_cum_bids,
+            impressions_blocked: data.impressions_all_cum - data.impressions_upto_here_cum,
+            wins_blocked: data.wins_all_cum - data.wins_upto_here_cum,
+            bids_blocked: data.bids_all_cum - data.bids_upto_here_cum,
+            revenue_blocked: data.revenue_all_cum - data.revenue_upto_here_cum,
+        };
+        Object.assign(res, {
+            impressions_block_portion: res.impressions_blocked / data.impressions_total,
+            wins_blocked_portion: res.wins_blocked / data.wins_total,
+            bids_block_portion: res.bids_blocked / data.bids_total,
+            revenue_blocked_portion: res.revenue_blocked / data.revenue_total,
 
-        data.impressions_block_portion = data.impressions_blocked / data.impressions_total
-        data.requests_blocked_portion = data.requests_blocked/ data.requests_total
-        data.bids_block_portion = data.bids_blocked/ data.bids_total
-        data.revenue_blocked_portion = data.revenue_blocked / data.revenue_total
-        this.queue(data)
+            impressions_total: data.impressions_total,
+            wins_total: data.wins_total,
+            bids_total: data.bids_total,
+            revenue_total: data.revenue_total,
+        })
+
+        this.queue(res)
     }))
 
-/*
-h.pipe(fastCsv.createWriteStream({ headers: true }))
-    .pipe(fs.createWriteStream('./data/cookie_sample5_h.csv', 'utf8')).on('finish', function () { console.log('pc sample h - done.') })
-*/
+
 j.pipe(fastCsv.createWriteStream({ headers: true }))
     .pipe(fs.createWriteStream('./data/cookie_sample5_j.csv', 'utf8')).on('finish', function () { console.log('pc sample j - done.') })
-/*
-var k = h
-    .pipe(through(function (data) {
-        data.sovrn_revenue = data.kb_code[0] == 'S' ? data.cpm : 0
-        this.queue(data);
-    }))
-    .pipe(gb.groupBy(['placement_id', 'uid'], true, {
-        session_cum_revenue: gb.sum('sovrn_revenue'),
-        session_cum_bids: gb.sum('sovrn_bids')
-    }))
-    .pipe(sort(comp(['ord', 'session_cum_bids'])))
-    
-    .pipe(gb.groupBy(['ord', 'session_cum_bids'], false, {
-        impressions: gb.count(),
-        bids: gb.sum('sovrn_bids'),
-        revenue: gb.sum('sovrn_revenue')
-    }))
 
-
-k.pipe(fastCsv.createWriteStream({ headers: true }))
-    .pipe(fs.createWriteStream('./data/cookie_sample5_k.csv', 'utf8')).on('finish', function () { console.log('pc sample k - done.') })
-*/
