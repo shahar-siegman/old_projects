2719cac87768007de545736c235479ab04e97557
Shahar EC Siegman <shahar-ec@komoona.com>
2017-05-17 13:58:37 +0000

    user filtering: tweaks to query runner code

--- kbidder/Node/user_filtering/queries/single_placement_sample.sql
+++ kbidder/Node/user_filtering/queries/single_placement_sample.sql
@@ -1,7 +1,8 @@
-insert into shahar_placement_user_sample
+insert into shahar_placement_user_sample1
 select "timestamp", 
 	placement_id, 
-	client_ip, 
+	client_ip,
+	geo_country,
 	uid, 
 	kb_code, 
 	kb_sold_cpm, 
@@ -9,7 +10,10 @@ select "timestamp",
 	rb_impression, 
 	pbsbids, 
 	hdbd_json, 
-	pc
+	pc,
+	filtered,
+	filter_reason,
+	flm
 from impressions i 
 where length(hdbd_json)>0
 and placement_id={placement_id}
--- kbidder/Node/user_filtering/run_redshift_placement_queries.js
+++ kbidder/Node/user_filtering/run_redshift_placement_queries.js
@@ -6,20 +6,20 @@ const d3 = require('d3-queue')
 const through = require('through')
 const queryString = require('./placement_query_string')
 
-var q = d3.queue(1)
+var q = d3.queue(6)
 
 
 fs.createReadStream('queries/sovrn_sampling_rates.csv', 'utf8')
     .pipe(fastCsv.parse({ headers: true }))
     .pipe(through(function (data) {
-        var queryParams = { placementId: data.placement_id, date: '2017-05-11', sampleRatio: data.hex2_sample_for_1000_wins }
+        var queryParams = { placementId: data.placement_id, date: '2017-05-15', sampleRatio: data.hex2_sample_for_1000_wins }
         console.log('queuing query: ' + JSON.stringify(queryParams))
         q.defer(
             function (params, callback) {
                 var rs = connections.kmnRedshift(),
                     fullQuery = queryString(params)
                 rs.setupDB();
-                console.log(fullQuery)
+                console.log((new Date).toISOString() + ': ' + fullQuery)
                 rs.runQuery(fullQuery, function (err, result) {
                     if (err)
                         console.log('Error: ' + err)
@@ -31,12 +31,13 @@ fs.createReadStream('queries/sovrn_sampling_rates.csv', 'utf8')
             },
             queryParams
         )
-    })).on('finish', function () {
+		this.queue(data)
+    })).on('end', function () {
         console.log('done queuing')
         q.await(function (error) {
             if (error)
                 throw error;
-            console.log('done running queued tasks')
+            console.log((new Date).toISOString() + ': ' +'done running queued tasks')
         })
     })
 
