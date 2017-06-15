b616e375babddebae773b300c465ba920f01cbb4
shahar.siegman <shahar@komoona.com>
2017-06-15 13:38:53 +0300

    improve generality of R regression code in prep for bid value regression

--- kbidder/Node/user_filtering/bidrate_sequential_prediction/extract_linear_coefficients1.R
+++ kbidder/Node/user_filtering/bidrate_sequential_prediction/extract_linear_coefficients1.R
@@ -1,18 +1,38 @@
 suppressMessages(library(dplyr))
+# some consts
+ouputFileExtensionString = '_coeffs_no_cookies_compare.csv'
+withCookie = F
+targetMetric = 'bid_rate'
+model1Regressors = c('bid_rate_so_far', 'wb',  'res', 'interaction_factor')
+model2Regressors = 'res'
+
+# no hoisting - helper functions need to go at the top
+applyRegression <- function (reg,x) {
+  intercept <- reg$coefficients[1]
+  reg <- reg$coefficients[2:(length(x)+1)]
+
+  return (sum(reg*x)+intercept)
+}
+# end helper functions
+
+# get arguments from the environment (if running from inside an r session)
 if (exists('externalArgs')) {
   args <- externalArgs
 } else {
+  # or, get variables from the command line
   args <- commandArgs(trailingOnly = TRUE)
 }
 argument1 <- args[1]
 argument2 <- args[2]
+argument3 <- args[3]
 
+# parse the input arguments, decide on input and output file names
 inputDir <- dirname(argument1)
 inputFile <- basename(argument1)
 inputFileNoExt <- strsplit(inputFile,'.',fixed=T)[[1]][1]
 outputFile <- ifelse(length(argument2)>3,
                      argument2,
-                     file.path(inputDir,paste0(inputFileNoExt,'_coeffs.csv')))
+                     file.path(inputDir,paste0(inputFileNoExt,ouputFileExtensionString)))
 
 print(paste0('argument1: ', argument1, '; out file: ', outputFile))
 print(paste0('directory: ', getwd()))
@@ -41,56 +61,46 @@ for (k in 1:length(networks)) {
   print(paste0('network: ', networks[k], '; placements: ', paste(placements,collapse=',')))
   for (i in 1:length(placements)) {
     current_placement <- levels(d$placement_id)[i]
-    #print(paste0("i=",i,"placement: ",current_placement))
 
     b1 <- d %>% filter(placement_id == current_placement,
                        !is_100percent_fill,
-                       has_cookie)
+                       has_cookie==withCookie)
     if (nrow(b1)<10 || nrow(b1 %>% filter(wb>0))<5) {
       print(paste0('b1: placement ', current_placement, ': ', nrow(b1), ' rows; ', nrow(b1 %>% filter(wb>0)), ' nonzero-wb rows'))
       reg1=list(coefficients=c(0,0,0,0,0),residuals=c(0,0))
     } else {
-      reg1 = lm(bid_rate ~ bid_rate_so_far + wb + res + interaction_factor, b1)
+      reg1 = lm(formula(paste0(targetMetric,' ~ ', paste0(reg1Regressors,collapse=' + '))), b1)
     }
-    #print(paste0('1: ', reg1$coefficients[1]))
-    #print(paste0('2: ', reg1$coefficients[2]))
-    #print(paste0('3: ', reg1$coefficients[3]))
-    #print(paste0('4: ', reg1$coefficients[4]))
-    #print(paste0('5: ', reg1$coefficients[5]))
 
     b2 <- d %>% filter(placement_id == current_placement,
                        is_100percent_fill,
-                       has_cookie)
+                       has_cookie == withCookie)
     if (nrow(b2)<10) {
       print(paste0('b2: placement ', current_placement, ': ', nrow(b2), ' rows'))
       reg2 = list(coefficients=c(0,0,0,0,0),residuals=c(0,0))
     } else {
-      reg2 = lm(bid_rate ~ res, b2)
+      reg2 = lm(formula(paste0(targetMetric,' ~ ', paste0(reg2Regressors,collapse=' + '))), b2)
     }
 
     row <- data.frame(network = networks[k],
-                      placement_id = current_placement,
-                      model1_bid_rate_so_far = reg1$coefficients[2],
-                      model1_bids_in_session = reg1$coefficients[3],
-                      model1_requests_in_session = reg1$coefficients[4],
-                      model1_bids_request = reg1$coefficients[5],
-                      model1_intercept = reg1$coefficients[1],
-                      model2_requests = reg2$coefficients[2],
-                      model2_intercept = reg2$coefficients[1],
-                      model1_rms = sqrt(mean(reg1$residuals^2)),
-                      model2_rms = sqrt(mean(reg2$residuals^2)),
-                      model1_5_0 = reg1$coefficients[1] + reg1$coefficients[4]*5,
-                      model1_5_5 = reg1$coefficients[1] +
-                        reg1$coefficients[2]+
-                        reg1$coefficients[3]*5 +
-                        reg1$coefficients[4]*5 +
-                        reg1$coefficients[5]*25,
-                      model2_5_5 = reg2$coefficients[1] +
-                        reg2$coefficients[2]*5)
+                      placement_id = current_placement)
+    for (m in 1:length(model1Regressors)) {
+      row[paste0("model1_",model1Regressors[m])] = reg1$coefficients[m+1]
+    }
+    row$model1_intercept = reg1$coefficients[1]
+    for (m in 1:length(model2Regressors)) {
+      row[paste0("model2_",model2Regressors[m])] = reg2$coefficients[m+1]
+    }
+    row$model2_intercept = reg2$coefficients[1]
+    row$model1_rms = sqrt(mean(reg1$residuals^2))
+    row$model2_rms = sqrt(mean(reg2$residuals^2))
+    row$model1_5_0 = applyRegression(reg1,c(0,0,5))
+    row$model1_5_5 = applyRegression(reg1,c(1,5,5,25))
+    row$model2_5_5 = applyRegression(reg2,c(5))
 
-    output <- rbind(output, row)
-    #row.names(output) <- levels(a$placement_id)
+output <- rbind(output, row)
   }
 }
 
 write.csv(output, outputFile, row.names = F)
+
